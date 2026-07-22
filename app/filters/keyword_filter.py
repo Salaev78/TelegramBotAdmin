38f48e4268dd1cbeb23f73f6a2b89bc8d6cc6427
Salaev78 <t.salaev@outlook.com>
2026-07-23 00:18:25 +0200

    Expand spam keyword detection

--- app/filters/keyword_filter.py
+++ app/filters/keyword_filter.py
@@ -2,12 +2,27 @@ print("keyword_filter loaded")
 from app.models.filter_result import FilterResult
 
 SPAM_WORDS = [
-    "crypto",
-    "bitcoin",
-    "usdt",
-    "onlyfans",
-    "porn",
-    "casino",
+        "crypto", "bitcoin", "btc", "eth", "ethereum",
+    "usdt", "usdc", "ton", "toncoin", "wallet",
+    "airdrop", "blockchain", "web3", "binance", "крипта", "биток",
+
+    # Казино / ставки
+    "casino", "aviator", "1win", "melbet", "parimatch",
+    "bet", "букмекер", "ставки", "казино",
+
+    # 18+
+    "onlyfans", "of", "nsfw", "porn", "sex", "escort", "cp",
+
+    # Работа / развод
+    "заработок", "доход", "без вложений", "работа онлайн",
+    "инвестиции", "пассивный доход",
+
+    # Telegram
+    "t.me/", "@",
+
+    # Спам
+    "пиши в лс", "в лс", "личные сообщения",
+    "пишите в личку", "пиши мне"
 ]
 
 
